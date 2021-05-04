0669ac1b9ee426674fd870635cfd2a34cea20b93
ASSASSIN21r <74041929+ASSASSIN21r@users.noreply.github.com>
2021-05-04 10:55:18 +0530

    Add files via upload

--- speech test2.py
+++ speech test2.py
@@ -9,11 +9,28 @@ with sr.Microphone() as source:
     if text == "new":
         print("new main created")
         text2="hi"
-        while text2!="exit":
+        while text2 != "exit":
             re = sr.Recognizer()
             with sr.Microphone() as sourcee:
              audio1 = re.listen(source)
             text2 = re.recognize_google(audio1)
-            print(text2)
+            if text2 == "print variable":
+                print("print("")")
+            elif text2 =="print text" :
+                print('print(" ")')
+            elif text2 =="input":
+                print('input("")')
+            elif text2 =="if else statement":
+                print('if  :   else:')
+            elif text2=="next":
+                print("\n")
+            elif text2=="for loop":
+                print("for x in  :")
+            else:
+
+                print(text2.lower())
+
+
+
     else:
        print("syntax error")
