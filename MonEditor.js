5a3990d70f1e40a24ccd876a6f35068c88845ea4
ASSASSIN21r <74041929+ASSASSIN21r@users.noreply.github.com>
2021-05-10 09:48:37 +0530

    Add files via upload

---
+++ MonEditor.js
@@ -0,0 +1,71 @@
+import React, {useRef} from 'react'
+import Editor from '@monaco-editor/react'
+
+export default function MonEditor(props) {
+
+    // const [editorContent, updateContent] = useState('')
+    var editorContent;
+    const editorIns = useRef()
+    const monacoIns = useRef()
+
+    
+    function handleMount(editor, monaco){
+        editorIns.current = editor
+        monacoIns.current = monaco
+    }
+
+    // function handleChange(value, event){
+        //getPosition is used to get the current position of cursore
+        //editor.getModel().getLineContent(line)
+        // updateContent(value)
+        // const position = editorIns.current.getPosition()
+        // console.log(position)
+        // console.log(value.split('\n'))
+
+    // }
+
+    function handleClick(){
+        // this is the function which starts python process 
+        // const position = editorIns.current.getPosition().lineNumber
+        window.myElect.notificationApi.sendNotification(["my custom notification"])
+    }
+
+    window.myElect.notificationApi.receiveNotificatoin("fromMain", (data)=>{
+        console.log(`Received ${data[0]} from main process`);
+        const pos = editorIns.current.getPosition();
+        const range = new monacoIns.current.Range(
+            pos.lineNumber,
+            pos.column,
+            pos.lineNumber,
+            pos.column
+        );
+        editorIns.current.executeEdits("new-bullets", [
+            { identifier: "new-bullet", range, text: data[0] }
+        ]);
+
+        editorIns.current.setPosition({
+            lineNumber: pos.lineNumber,
+            column: pos.column + data[1]
+        });
+        editorIns.current.focus();
+    })
+
+    
+    
+
+    return (
+        <>
+            <button onClick={()=>handleClick()}>Click Me</button>
+            
+            <Editor
+                height="90vh"
+                defaultLanguage="python"
+                defaultValue="'''some comment'''"
+                value={editorContent}
+                onMount={handleMount}
+                
+            />
+            
+        </>
+    )
+}
